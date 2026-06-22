39280de18178a969bf827d3e0d7337cec2a0f942
Raghulresi <raghulprakash848@gmail.com>
2026-06-22 12:03:47 +0530

    revised

--- src/Use_Context_hook.js
+++ src/Use_Context_hook.js
@@ -1,19 +1,18 @@
-import { useState,createContext,useContext } from "react";
-
-const UserContext =  createContext();
+import { useState,createContext, useContext } from "react";
+const userContext = createContext();
 function Use_Context_hook()
 {
-    const [name,setName] = useState("Mac!!");
+    const [name , setName] = useState("Mac!!");
     return(
         <div>
-            <UserContext.Provider value={name}>
-                <h1>useContext_hook: </h1>
+            <userContext.Provider value={name}>
                 <h1>Component0 {name}</h1>
                 <Component1/>
-            </UserContext.Provider>
+            </userContext.Provider>
         </div>
     );
 }
+
 function Component1()
 {
     return(
@@ -23,11 +22,14 @@ function Component1()
         </div>
     );
 }
+
 function Component2()
 {
-    const name = useContext(UserContext);
+    const name = useContext(userContext);
     return(
-        <h1>Component2 {name}</h1>
+        <div>
+            <h1>Component2 {name}</h1>
+        </div>
     );
 }
 export default Use_Context_hook;
--- src/Use_effect_hook.js
+++ src/Use_effect_hook.js
@@ -2,11 +2,15 @@ import { useEffect, useState } from "react";
 
 function Use_effect_hook()
 {
-    const [count,setCount] = useState(0);
-    useEffect(()=>{
-        setTimeout(()=>{setCount((count)=>count+1);}
-                   ,1000);
+    const [count, setCount] = useState(0);
+    useEffect(()=>
+    {
+        setTimeout(()=>{setCount((count)=>count+1);},1000)
     });
-    return <h1>useEffect_hook: Counting {count}</h1>
+    return(
+        <div>
+            <h1>useEffect_hook:{count} </h1>
+        </div>
+    );
 }
 export default Use_effect_hook;
